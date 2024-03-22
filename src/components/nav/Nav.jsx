d4d28f2e7f1689a573846798504c46bd2c02d6f2
Evans Kofi Nyamekye <nyamekye2131@gmail.com>
2024-03-22 15:47:41 +0000

    Add Header and Nav components

---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,12 @@
+import { React } from "react";
+import './nav.css'
+import { AiOutlineHome } from "react-icons/ai";
+
+const Nav = () => {
+  return (
+    <nav>
+      <a href="#"><AiOutlineHome /></a>
+    </nav>
+  )
+}
+export default Nav
