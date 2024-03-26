60db200bb2503e1132859dd0986cbbbf14021b7f
Evans Kofi Nyamekye <nyamekye2131@gmail.com>
2024-03-26 02:43:40 +0000

    Add  component files to main development

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,10 +1,126 @@
-import React from 'react'
-import './experience.css'
+import React from "react";
+import "./experience.css";
+import { BsFillPatchCheckFill } from "react-icons/bs";
 
 const Experience = () => {
   return (
-    <div>Experience</div>
-  )
-}
+    <section id="experience">
+      <h5>What Skill I have</h5>
+      <h2>My Experience</h2>
 
-export default Experience
+      <   div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>React</h4>
+                <small className="text-light">Expeprience</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>HTML</h4>
+                <small className="text-light">Experience</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>CSS</h4>
+                <small className="text-light">Experience</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>Bootstrap</h4>
+                <small className="text-light">Experience</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>Tailwind</h4>
+                <small className="text-light">Experience</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>Ruby</h4>
+                <small className="text-light">Experience</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>Svelte</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+          </div>
+        </div>
+
+        <div className="experience__frontend">
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>PHP</h4>
+                <small className="text-light">Basic</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>Node JS</h4>
+                <small className="text-light">Experience</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>PostgreSQL</h4>
+                <small className="text-light">Experience</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>MySQL</h4>
+                <small className="text-light">Experience</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>Java</h4>
+                <small className="text-light">Experience</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>Ruby on Rails</h4>
+                <small className="text-light">Experience</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>Python</h4>
+                <small className="text-light">Basic</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Experience;
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 import './footer.css'
+import { FaFacebookF } from 'react-icons/fa'
+import { IoLogoTwitter } from 'react-icons/io'
+import { FiInstagram } from 'react-icons/fi'
 
-const Footer = () => {
+const Footer = () => { 
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>Evans Portfolio</a>
+
+      <ul className='permalinks'>
+          <li><a href="#">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#experience">Experience</a></li>
+          <li><a href="#service">Service</a></li>
+          <li><a href="#portfolio">Portfolio</a></li>
+          <li><a href="#contact">Contact</a></li>
+      </ul> 
+
+      <div className="footer__socials">
+        <a href="https://facebook.com" ><FaFacebookF /></a>
+        <a href="https://twitter.com" ><IoLogoTwitter /></a>
+        <a href="https://instagram.com" ><FiInstagram  /></a>
+      </div>
+
+      <div className="footer__copyright">
+          <small>&copy; EVANS Portfolio. All right reserved.</small>
+      </div>
+    </footer>
   )
 }
 
