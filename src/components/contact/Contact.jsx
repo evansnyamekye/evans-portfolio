81a593951b506c8329759f8b6960f5533a1f0649
Evans Kofi Nyamekye <nyamekye2131@gmail.com>
2024-03-26 13:05:02 +0000

    Update: modified the packages.json file and add emailjs

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,10 +3,29 @@ import './contact.css'
 import { MdOutlineEmail } from "react-icons/md";
 import { RiMessengerLine } from "react-icons/ri";
 import { FaWhatsapp } from "react-icons/fa";
-
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
 
 
 const Contact = () => {
+  
+  const form = useRef(); 
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+    .sendForm('service_7fxtau8', 'template_vwfetf4', form.current, 'izYC5cXhmXbTKze1r')
+    .then(
+      () => { 
+        console.log('SUCCESS!'); 
+      },
+      (error) => {
+        console.log('FAILED...', error.text);
+      },
+    );
+  } 
+
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
@@ -33,7 +52,7 @@ const Contact = () => {
               <a href="https://api.whatsapp.com/send?phone+233530646942" target='_blank'>Send a message</a>
           </article>
         </div>
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name='name' placeholder='Your Full Name' required/>
           <input type="email" name='email' placeholder='Your Email' required/>
           <textarea name="message" rows="7" placeholder="Your Message" required></textarea>
